740b24645e018965c9b884ed84db5539e89b7792
Vinícius <vinicius.raymundo@zup.com.br>
2021-10-27 11:34:37 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/Zupcar/ZupcarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.Zupcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
