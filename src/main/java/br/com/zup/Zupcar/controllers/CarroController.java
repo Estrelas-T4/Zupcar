41ad1b23c63de81e8665e8745b18edae8d4a14ae
Vinícius <vinicius.raymundo@zup.com.br>
2021-10-27 14:46:25 -0300

    Implementar lista para armazenar carros

--- src/main/java/br/com/zup/Zupcar/controllers/CarroController.java
+++ src/main/java/br/com/zup/Zupcar/controllers/CarroController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping("/carros")
 public class CarroController {
-
+    private List<CarroDTO> concessionaria = new ArrayList<>();
 
 }
