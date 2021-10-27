d206c4106c792dd4d8b71bd4feddd2839ff502a0
Vinícius <vinicius.raymundo@zup.com.br>
2021-10-27 15:54:18 -0300

    Adiconar rotas de cadastro para carros

--- src/main/java/br/com/zup/Zupcar/controllers/CarroController.java
+++ src/main/java/br/com/zup/Zupcar/controllers/CarroController.java
@@ -1,17 +1,24 @@
 package br.com.zup.Zupcar.controllers;
 
 import br.com.zup.Zupcar.dtos.CarroDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/carros")
+@RequestMapping("/carros") // Mapea as requisições para o endpoint nele contido
 public class CarroController {
     private List<CarroDTO> concessionaria = new ArrayList<>();
 
+    @GetMapping // é o Request Mapping utilizando o Verbo GET do PROTOCOLO HTTP
+    public List<CarroDTO> exibirTodosOsCarros(){
+        return concessionaria;
+    }
+
+    @PostMapping // é o Request Mapping utilizando o Verbo POST do PROTOCOLO HTTP
+    public void cadastrarCarro(@RequestBody CarroDTO carroDTO){
+        // Todo Classe DTO são representações de Json seja de Entrada ou Saida.
+        concessionaria.add(carroDTO);
+    }
 }
